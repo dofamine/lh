2720e99649fcc8359033ffd83e45f5943839db42
Daria Volkova <dvolkova@leasehawk.com>
2021-10-13 01:36:14 +0300

    clean up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'customers',
-    loadChildren: () =>
-      import('./customers/customers.module').then((module) => module.CustomersModule),
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/customers/customers.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'lh-customers',
-  templateUrl: './customers.component.html',
-  styleUrls: ['./customers.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class CustomersComponent {}
