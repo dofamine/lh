7b5085a464df4e49e93d0f8cd829d10ed9466bc1
Daria Volkova <dvolkova@leasehawk.com>
2021-10-14 18:39:33 +0300

    primeng components styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,16 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lease-hawk';
+  date: Date | null = null;
+  selectedCity: { name: string; code: string } | null = null;
+  selectedCities: { name: string; code: string }[] | null = null;
+  cities = [
+    { name: 'New York', code: 'NY' },
+    { name: 'Rome', code: 'RM' },
+    { name: 'London', code: 'LDN' },
+    { name: 'Istanbul', code: 'IST' },
+    { name: 'Paris', code: 'PRS' },
+  ];
 
   some(): void {
     return;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { CalendarModule } from 'primeng/calendar';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, ButtonModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ButtonModule,
+    DropdownModule,
+    FormsModule,
+    MultiSelectModule,
+    CalendarModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
