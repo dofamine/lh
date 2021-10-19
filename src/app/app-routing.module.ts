248e81c592478909b7696eae2f78a2e89d5c9c54
Daria Volkova <dvolkova@leasehawk.com>
2021-10-19 15:50:39 +0300

    added footer and modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,14 @@ const routes: Routes = [
     path: LH_ROUTES.dashboard,
     loadChildren: () => import('./pages/dashboard/dashboard.module').then((module) => module.DashboardModule),
   },
+  {
+    path: LH_ROUTES.notFound,
+    loadChildren: () => import('./pages/not-found/not-found.module').then((module) => module.NotFoundModule),
+  },
+  {
+    path: LH_ROUTES.forbidden,
+    loadChildren: () => import('./pages/forbidden/forbidden.module').then((module) => module.ForbiddenModule),
+  },
 ];
 
 @NgModule({
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -4,10 +4,11 @@ import { HeaderModule } from './header/header.module';
 import { NavigationModule } from './navigation/navigation.module';
 import { RouterModule } from '@angular/router';
 import { FiltersModule } from './filters/filters.module';
+import { FooterModule } from './footer/footer.module';
 
 @NgModule({
   declarations: [LayoutComponent],
   exports: [LayoutComponent],
-  imports: [HeaderModule, NavigationModule, RouterModule, FiltersModule],
+  imports: [HeaderModule, NavigationModule, RouterModule, FiltersModule, FooterModule],
 })
 export class LayoutModule {}
