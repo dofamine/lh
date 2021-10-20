8281c3ebe1136e2154d78c4fadfa50c9ae65dbcc
Daria Volkova <dvolkova@leasehawk.com>
2021-10-20 12:48:45 +0300

    moved footer & added filters

--- src/app/layout/filters/filters.component.ts
+++ src/app/layout/filters/filters.component.ts
@@ -1,4 +1,5 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'lh-filters',
@@ -6,4 +7,35 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./filters.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FiltersComponent {}
+export class FiltersComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
+
+  periods = [
+    { name: 'Yesterday', value: 1 },
+    { name: 'Last 30 days', value: 2 },
+    { name: 'Last 60 days', value: 3 },
+    { name: 'Last 90 days', value: 4 },
+  ];
+  accounts = [
+    { name: 'Account 1', value: 1 },
+    { name: 'Account 2', value: 2 },
+    { name: 'Account 3', value: 3 },
+    { name: 'Account 4', value: 4 },
+  ];
+  sites = [
+    { name: 'Site 1', value: 1 },
+    { name: 'Site 2', value: 2 },
+    { name: 'Site 3', value: 3 },
+    { name: 'Site 4', value: 4 },
+  ];
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(): void {
+    this.form.addControl('periods', new FormControl());
+    this.form.addControl('accounts', new FormControl());
+    this.form.addControl('sites', new FormControl());
+  }
+}
--- src/app/layout/filters/filters.module.ts
+++ src/app/layout/filters/filters.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FiltersComponent } from './filters.component';
+import { DropdownModule } from 'primeng/dropdown';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [FiltersComponent],
   exports: [FiltersComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, DropdownModule, ReactiveFormsModule, MultiSelectModule, ButtonModule],
 })
 export class FiltersModule {}
